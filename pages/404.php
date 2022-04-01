0bbcac66a49ac0d0aa2fe1b03ae621df5fd6843a
Hudson Nascimento de Lima <51245039+HudsonnLima@users.noreply.github.com>
2022-04-01 18:59:56 -0300

    Create 404.php

---
+++ pages/404.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>ERRO 404</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<div>ESTÁ PÁGINA NÃO EXISTE</div>
+
+</body>
+</html>
